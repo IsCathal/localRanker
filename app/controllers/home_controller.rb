6b698ede180770150e0483d0b9d20e760f8227f8
IsCathal <mccabemcathal@gmail.com>
2024-06-29 13:05:13 +0100

    first commit

---
+++ app/controllers/home_controller.rb
@@ -0,0 +1,29 @@
+ 
+class HomeController < ApplicationController
+  
+  def show
+   
+
+  end
+
+  def index
+  end
+
+  def new
+   
+
+  end
+
+  def edit
+	
+  end
+
+  def create
+   
+  end
+
+
+
+
+
+end
